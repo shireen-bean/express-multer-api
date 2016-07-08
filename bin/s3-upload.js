0849dcd4178dd36def9b04c37470410f17cc7d0d
shkherad <shkherad@bu.edu>
2016-07-08 13:09:38 -0400

    Determine file/mime type, start AWS upload funxn
    Use the file-type module to determine the content type of the upload
    
    Configured options for our upload base on our bucket name.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,8 +1,31 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
 
 let filename = process.argv[2] || '';
+
+// creating helper function in case file-type cant identify type of file-type
+//helper will assugn default and must return object
+
+const mimeType = (data) => {
+  return Object.assign({ // second arg overwrites first arg
+    ext: 'bin',
+    mime: 'application/octet-stream', //set defaults
+  }, fileType(data)); //overwrites defaults
+};
+
+const awsUpload = (file) => {
+  const options = {
+    ACL: "public-read",
+    Body: file.data,
+    Bucket: 'shireen-wdi-bucket',
+    ContentType: file.mime,
+    Key: `test/test.${file.ext}`
+  };
+  return Promise.resolve(options);
+  //return options
+};
 
 const readFile = (filename) => {
   return new Promise ((resolve, reject)=>{
@@ -16,8 +39,17 @@ const readFile = (filename) => {
     });
   });
 
-}
+};
+
+
 
 readFile(filename)
-.then((data)=> console.log( `${filename} is ${data.length} bytes long`))
+.then((data)=> {
+  let file = mimeType(data);
+  file.data = data;
+  return file;
+}).then(awsUpload)
+.then(console.log)
 .catch(console.error);
+
+//console.log( `${filename} is ${data.length} bytes long`)
